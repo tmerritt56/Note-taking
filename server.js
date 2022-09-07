bb9e08f0b3462c3b5c776ad2911b9947e2c6eb37
tmerritt56 <104723943+tmerritt56@users.noreply.github.com>
2022-09-06 21:36:03 -0500

    test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const htmlRoute = require('./routes/htmlroutes');
 const apiRoute = require('./routes/apiroutes');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 // middleware
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
